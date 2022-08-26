7e1b8f8ca1b6d78bc4f14602f8b5a1d8eda859a6
Jamie-Carter-GitHub <jamie.carter20@hotmail.co.uk>
2022-08-26 22:16:14 +0100

    Record Adding
    added functionality to add a record to the database model using a html form, and redirect back to the main page once this is completed.

--- djangoproject/sayhello/views.py
+++ djangoproject/sayhello/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from .models import Items
+from django.urls import reverse
  
 def index (request) :
     items = Items.objects.all().values()
@@ -10,3 +11,14 @@ def index (request) :
         'items' : items,
     }
     return HttpResponse(template.render(context, request))
+
+def add(request):
+  template = loader.get_template('add.html')
+  return HttpResponse(template.render({}, request))
+
+def addrecord(request):
+    name = request.POST['name']
+    desc = request.POST['desc']
+    item = Items(itemname=name, itemdesc=desc)
+    item.save()
+    return HttpResponseRedirect(reverse('index'))
