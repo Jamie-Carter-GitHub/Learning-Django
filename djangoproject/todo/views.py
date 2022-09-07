370b19d332cd1bdbfc062e1501d7216e8f96b554
Jamie-Carter-GitHub <jamie.carter20@hotmail.co.uk>
2022-09-07 21:19:51 +0100

    CSS and to-do input form
    Finished the grid for displaying (currently) up to 4 tasks per page.
    created the form to add and save new tasks.

--- djangoproject/todo/views.py
+++ djangoproject/todo/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
+from .models import Tasks
 
 def index (request) :
     template = loader.get_template('todoIndex.html')
@@ -9,3 +10,10 @@ def index (request) :
         'count': 0,
     }
     return HttpResponse(template.render(context, request))
+
+def addtodo(request):
+    name = request.POST['name']
+    desc = request.POST['desc']
+    task = Tasks(taskname=name, taskdesc=desc)
+    task.save()
+    return HttpResponseRedirect(reverse('index'))
