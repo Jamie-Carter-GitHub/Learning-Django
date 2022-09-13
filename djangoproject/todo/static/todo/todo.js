744d082268aaba502668651ee1ca1bdc0b56eb22
Jamie-Carter-GitHub <jamie.carter20@hotmail.co.uk>
2022-09-13 19:32:17 +0100

    Added functionality to next and previous buttons
    The next and previous buttons now correctly switch out the tasks so that new ones are shown as expected.

--- djangoproject/todo/static/todo/todo.js
+++ djangoproject/todo/static/todo/todo.js
@@ -1,13 +1,21 @@
-var page=1; //keep track of page number for managaing which tasks are shown.
+var page=0; //keep track of page number for managaing which tasks are shown.
+
+function defaultPage(){
+	var divs = ['#topLeftTask','#topRightTask','#botLeftTask','#botRightTask'];
+	for(i = 0; i < divs.length; i++){
+		$(divs[i]).html("<h3>Tasks will show up here once more are added.</h3>");
+	}
+}
 
 function populatePage(){
-	if(page > 1){
+	if(page > 0){
 		$(previousButton).prop('disabled', false)
 	}
     var divs = ['#topLeftTask','#topRightTask','#botLeftTask','#botRightTask'];
 	//wait for AJAX to finish
 	$.when(getTasks()).done(function (tasks){
 
+		tasks = tasks.slice(page*4);
 		$.each(tasks, function(index, task){
 			if(index >= 4){
 				$(nextButton).prop('disabled', false)
@@ -17,9 +25,6 @@ function populatePage(){
             strResult = strResult + 
             "<h3>" + task.taskname + "</h3>" +
             "<p>" + task.taskdesc + "</p>"
-            console.log(divs[index]);
-			console.log(task.taskname);
-            console.log(task.taskdesc);
             $(divs[index]).html(strResult);
 		});
 		
@@ -38,16 +43,16 @@ function getTasks()
 
 function previous()
 {
-	console.log("Previous");
 	page--;
 	$(previousButton).prop('disabled', true)
+	defaultPage();
 	populatePage();
 }
 
 function next()
 {
-	console.log("Next");
 	page++;
 	$(nextButton).prop('disabled', true)
+	defaultPage();
 	populatePage();
 }
